06991263b4966b5ea64cf515c334fb7c9eab4707
Christian Serrano <cbi2243012797@izt.uam.mx>
2026-07-15 17:03:09 -0600

    Agregando la interfaz de Cola

--- Listas/src/Cola.java
+++ Listas/src/Cola.java
@@ -1,2 +1,11 @@
-public interface Cola {
+public interface Cola <E> extends Iterable<E>  {
+    
+    public void encolar (E elemento);
+
+    E desencolar();
+    E consultarFrente();
+    boolean esVacia();
+    int numElementos();
+    void limpiar();
+    
 }
